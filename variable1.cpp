52aafd00afa64169779f2e5c6f728b02c674e3ec
istpro <educarl.set@gmail.com>
2023-05-07 03:23:24 +0300

    Main Variable day 1

---
+++ variable1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+int main(){
+
+
+    //int [ whole number]
+    int x = 5;
+    //double [ decimal]
+    double y = 3.127;
+    //char [ single charactor]
+    char  grade = 'A';
+    //bool [ true or falce]
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+    //string [sequense of text]
+    std::string name = "Ashvin";
+    std::string day = "Sunday";
+
+
+    std::cout<<x<<std::endl;
+    std::cout<<y<<std::endl;
+    std::cout<<grade<<std::endl;
+    std::cout<<student<<std::endl;
+    std::cout<<power<<std::endl;
+    std::cout<<forSale<<std::endl;
+
+    return 0;
+}
